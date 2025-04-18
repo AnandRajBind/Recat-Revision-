c5e6723e85009bf978c601524f873ab4244fde8a
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-04-18 23:26:14 +0530

    18/04/2024
    Create a Card (Used to React component and props )

--- React2/Components/src/App.jsx
+++ React2/Components/src/App.jsx
@@ -24,9 +24,63 @@
 
 
 import React from 'react'
-
+import { Card } from './components/Card'
+ 
 export const App = () => {
+
+  const user = [
+    {
+      "Name": "Aarav Mehta",
+      "City": "Mumbai",
+      "Age": 28,
+      "Profession": "Software Engineer",
+      "ProfilePhoto": "https://randomuser.me/api/portraits/men/45.jpg"
+    },
+    {
+      "Name": "Nisha Verma",
+      "City": "Delhi",
+      "Age": 25,
+      "Profession": "Graphic Designer",
+      "ProfilePhoto": "https://randomuser.me/api/portraits/women/65.jpg"
+    },
+    {
+      "Name": "Rahul Singh",
+      "City": "Bangalore",
+      "Age": 32,
+      "Profession": "Product Manager",
+      "ProfilePhoto": "https://randomuser.me/api/portraits/men/22.jpg"
+    },
+    {
+      "Name": "Priya Sharma",
+      "City": "Jaipur",
+      "Age": 29,
+      "Profession": "Marketing Executive",
+      "ProfilePhoto": "https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+      "Name": "Karan Patel",
+      "City": "Ahmedabad",
+      "Age": 35,
+      "Profession": "Data Analyst",
+      "ProfilePhoto": "https://randomuser.me/api/portraits/men/38.jpg"
+    }
+  ]
+
+  user.forEach(() => {
+    console.log("sdfddsf");
+
+  })
   return (
-    <div>App</div>
+    <>
+      <div className='p-10 '>
+        {/* <Card user='Anand' age='24' city='Jaunpur' /> */}
+        {user.map(function (elem, idx) {
+          return <Card key={idx} user={elem.Name} age={elem.Age} city={elem.City} photo={elem.ProfilePhoto} Profession={elem.Profession}/>
+        })}
+
+
+      </div>
+    </>
   )
 }
+export default App
--- React2/Components/src/components/Card.jsx
+++ React2/Components/src/components/Card.jsx
@@ -14,12 +14,23 @@ console.log(props.name);// here we can see the props name is Anand
  export default Card
 
 */
-
-
 import React from 'react'
 
-export const Card = () => {
-  return (
-    <div>Card</div>
-  )
+export const Card = (props) => {
+
+    
+    return (
+        <div className='bg-white mr-8 mb-8 text-black inline-block p-6 text-center rounded'>
+            <img  className=" ml-8 h-32 w-32 rounded-full mb-3 "
+             src={props.photo} alt="Image Not Found" />
+            <h1 className='text-2xl font-bold mb-4'>
+                {props.user}  
+            </h1>
+
+            <h2 className='text-blue-400'>{props.Profession}</h2>
+            <h2 className='text-xl'>City: {props.city} </h2>
+            <h2 className='text-xl'> Age: {props.age} </h2>
+            <button className=' mt-5  bg-emerald-600 text-white px-4 py-2 rounded font-medium'>Add Friend </button>
+        </div>
+    )
 }
